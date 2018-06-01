13c910789ac89da396079c3fb1e85f2163afd29a
IamLAM <ingeniero@luismonge.com.mx>
2018-05-31 23:00:11 -0700

    Comparar resultados y desplegar

---
+++ 02_Condiciones/condiciones.py
@@ -0,0 +1,14 @@
+print("Trabajando con operaciones en python")
+
+m=1;
+j=5;
+i=m+j;
+
+if m<j:
+	print("M es menor")
+else: 
+	print ("M es mayor")
+	
+	
+#no existe switch en python como en otros lenguajes como c,c++,java,JS
+#Los operadores son igual < == >= <=
