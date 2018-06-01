1f59bd46a7a4f0d8d8459b249828fe1bc6cc5a98
IamLAM <ingeniero@luismonge.com.mx>
2018-05-31 23:19:16 -0700

    Ciclos for..in, while , range

---
+++ 04_Ciclos/Ciclos.py
@@ -0,0 +1,21 @@
+print("Ciclos con Python")
+
+
+for i in range(2,100):
+	print(i)
+	
+
+animales=["perro","gato","jirafa","hipopotamo","avestruz"];
+
+for i in animales:
+	print(i)
+	
+	#El lenguaje python no contempla do while 
+	
+	i=0
+	while i<10:
+		print(i)
+		#i++  //no funciona en python
+		i=i+1
+		
+		
