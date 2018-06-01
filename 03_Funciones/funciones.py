a4fdb5866d90e7b3b2fc4b7497e416fca9d37deb
IamLAM <ingeniero@luismonge.com.mx>
2018-05-31 23:08:23 -0700

    Funciones con parametro
    Usando return y vacias

---
+++ 03_Funciones/funciones.py
@@ -0,0 +1,25 @@
+
+print("Operaciones básicas")
+
+def suma(a,b):
+
+	return a+b
+	
+	
+r=suma(5,6)
+print(r)
+
+def resta():
+	
+	return 5-6
+	
+r=resta()
+print(r)
+
+def despedir():
+	print("Gracias nos vemos pronto")
+	
+	
+despedir()
+
+	
