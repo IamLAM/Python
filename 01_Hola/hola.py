90dee62f3024cccbf524e6c1af7757870382d114
IamLAM <ingeniero@luismonge.com.mx>
2018-05-31 22:52:42 -0700

    Diccionarios y listas
    Se añadio otro tipo de datos

--- 01_Hola/hola.py
+++ 01_Hola/hola.py
@@ -4,3 +4,16 @@ m=1;
 j=5;
 i=m+j;
 print("Resultado "+str(i))
+
+#diccionario
+d={'nombre':'Pilar','edad':30}
+print(d['nombre'])
+
+#listas
+
+l=[5,8,3000,1,3,8,6]
+print(l)
+l.sort()
+print(l)
+l.reverse()
+print(l)
